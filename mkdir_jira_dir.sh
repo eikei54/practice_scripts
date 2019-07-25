8577f82fd701fdc7bdc7daf6eaf41e749cfb88d5
Akihiro Kamimura <Akihiro.Kamimura@wdc.com>
2019-07-26 05:45:55 +0900

    update mkdir_jira to removie foldername from the target path

--- mkdir_jira_dir.sh
+++ mkdir_jira_dir.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
-SOURCE_PATH="/x/fwtest/Traces/L2/p6s/Integrity_CIV-X04/9JA0742A_07-18-19_18-23-51/errors/Error_000_ERR_COMMAND_TIMEOUT"
+#SOURCE_PATH="/x/fwtest/Traces/L2/p6s/Integrity_CIV-X04/9JA0742A_07-18-19_18-23-51/errors/Error_000_ERR_COMMAND_TIMEOUT"
 WORK_PATH="/d/work/FA"
 
+jira_no=$1
+SOURCE_PATH=$2
 
 #echo $SOURCE_PATH
 JIRA_PATH=`echo $SOURCE_PATH | sed -e "s:^\(.*\)/.*:\1:"`
-echo $JIRA_PATH
 JIRA_PATH=`echo $JIRA_PATH | sed -e "s:^.*Traces::"`
+echo $JIRA_PATH
 
 #echo "/x/fwtest/Traces/L2/p6s/Integrity_CIV-X04/9JA0742A_07-18-19_18-23-51/errors/Error_000_ERR_COMMAND_TIMEOUT" | sed -e "s:^.*Traces::"
 
-jira_no=$1
-TARGET_PATH=$WORK_PATH
+TARGET_PATH=$WORK_PATH"/"
 TARGET_PATH+=$jira_no
 TARGET_PATH+=$JIRA_PATH
 
 #echo $jira_no
 echo $TARGET_PATH
 
-#if [ ! -e $TARGET_PATH ]; then
-#    mkdir -p $TARGET_PATH
-#fi
+if [ ! -e $TARGET_PATH ]; then
+    mkdir -p $TARGET_PATH
+fi
 
-#cp -rp $SOURCE_PATH $TARGET_PATH
+cp -rp $SOURCE_PATH $TARGET_PATH
 
