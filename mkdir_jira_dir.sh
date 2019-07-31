f7079ebf9f8283bc605bce0a2662e17c1b8eb31a
Akihiro Kamimura <Akihiro.Kamimura@wdc.com>
2019-07-31 13:01:55 +0900

    updated to add parameter check

--- mkdir_jira_dir.sh
+++ mkdir_jira_dir.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 #SOURCE_PATH="/x/fwtest/Traces/L2/p6s/Integrity_CIV-X04/9JA0742A_07-18-19_18-23-51/errors/Error_000_ERR_COMMAND_TIMEOUT"
+
+
 WORK_PATH="/d/work/FA"
 
 jira_no=$1
 SOURCE_PATH=$2
+
+# check the number of arguments
+if [ $# -ne 2 ]; then
+    echo "parameter number; $#" 1>&2
+    echo "paramter nums to be 2" 1>&2
+    exit 1
+fi
+
 
 #echo $SOURCE_PATH
 JIRA_PATH=`echo $SOURCE_PATH | sed -e "s:^\(.*\)/.*:\1:"`
