2e4db62470e24d81c7215e8ef6b350bdae8aa426
Akihiro Kamimura <Akihiro.Kamimura@wdc.com>
2019-08-01 11:37:53 +0900

    updated to input JIRA number without "CCB8-"

--- mkdir_jira_dir.sh
+++ mkdir_jira_dir.sh
@@ -5,7 +5,7 @@
 
 WORK_PATH="/d/work/FA"
 
-jira_no=$1
+jira_no="CCB8-"$1
 SOURCE_PATH=$2
 
 # check the number of arguments
@@ -15,11 +15,14 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+#
+# Get original path to be used in the local foler
+#
 
 #echo $SOURCE_PATH
 JIRA_PATH=`echo $SOURCE_PATH | sed -e "s:^\(.*\)/.*:\1:"`
 JIRA_PATH=`echo $JIRA_PATH | sed -e "s:^.*Traces::"`
-echo $JIRA_PATH
+#echo $JIRA_PATH
 
 #echo "/x/fwtest/Traces/L2/p6s/Integrity_CIV-X04/9JA0742A_07-18-19_18-23-51/errors/Error_000_ERR_COMMAND_TIMEOUT" | sed -e "s:^.*Traces::"
 
@@ -27,8 +30,13 @@ TARGET_PATH=$WORK_PATH"/"
 TARGET_PATH+=$jira_no
 TARGET_PATH+=$JIRA_PATH
 
+#
+# Get original path to be used in the local foler
+#
+echo "Copying X_Drive data to ...."
+TEXT_TO_CONSOLE="Copied To:"
 #echo $jira_no
-echo $TARGET_PATH
+echo $TEXT_TO_CONSOLE$TARGET_PATH
 
 if [ ! -e $TARGET_PATH ]; then
     mkdir -p $TARGET_PATH
